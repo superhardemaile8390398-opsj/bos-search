6696c40a35a7ad14714853bd46682e7743a914aa
superhardemaile8390398-opsj <superhardemaile8390398@gmail.com>
2025-12-28 22:15:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open('bos').then(c=>c.addAll([
-    './','./index.html','./manifest.json','./game/index.html'
-  ])));
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  // simple pass-through, no caching yet
 });
